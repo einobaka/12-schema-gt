3012c601a92ad4b1c3a99e9a7407ff01f01d932b
einobaka <61556425+einobaka@users.noreply.github.com>
2020-08-26 22:15:01 -0700

    arrangement of employees

--- db/schema.sql
+++ db/schema.sql
@@ -5,26 +5,23 @@ USE employeeDB;
 
 CREATE TABLE department
 (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY,
     name VARCHAR(30),
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role
 (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_ID INT,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee
 (
-	id INT AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT ,
-    manager_id INT ,
-    PRIMARY KEY (id)
+    role_id INT,
+    manager_id INT,
 );
--- utils/dbcontrol.js
+++ utils/dbcontrol.js
@@ -1,93 +1,107 @@
 const inquirer = require('inquirer');
 const database = require('../db/db');
-const uuid = require('uuid');
 
 class DbMethod {
 
     addEmp() {
-        inquirer.prompt(
+
+        let managers = ['Joe Regal', 'Nate Avery', 'Mike Michaels']
+        let employee = [
+            // {
+            //     type: "input",
+            //     name: "first_name",
+            //     message: "Please enter the employee's first name.",
+            // },
+            // {
+            //     type: "input",
+            //     name: "last_name",
+            //     message: "Please enter the employee's last name.",
+            // },
+            // {
+            //     type: "list",
+            //     name: "title",
+            //     message: "Please select the employee's role",
+            //     choices: [
+            //         "General Manager",
+            //         "Sales Manager",
+            //         "Salesman",
+            //         "Service Manager",
+            //         "Service Advisor",
+            //         "Technician",
+            //         "Parts Manager",
+            //         "Parts Advisor",
+            //         "Accounting Controller",
+            //         "Receivables Clerk",
+            //         "Payables Clerk",
+            //         "Cashier"
+            //     ]
+            // },
+            {
+                type: "list",
+                name: "manager",
+                message: "Please select the employee's manager",
+                choices: managers
+            }
+
+        ];
+
+        inquirer.prompt(employee)
             // "Add Department",
             // "Add Role", 
-            [
-                {
-                    type: "input",
-                    name: "first_name",
-                    message: "Please enter the employee's first name.",
-                },
-                {
-                    type: "input",
-                    name: "last_name",
-                    message: "Please enter the employee's last name.",
-                },
-                /////////
-                {
-                    type: "list",
-                    name: "title",
-                    message: "Please select the employee's role",
-                    choices: [
-                        "General Manager",
-                        "Sales Manager",
-                        "Salesman",
-                        "Service Manager",
-                        "Service Advisor",
-                        "Technician",
-                        "Parts Manager",
-                        "Parts Advisor",
-                        "Accounting Controller",
-                        "Receivables Clerk",
-                        "Payables Clerk",
-                        "Cashier"
-                    ]
-                },
-                {
-                    type: "number",
-                    name: "salary",
-                    message: "Please enter the employee's salary.",
-                },
-                ////////////
-                {
-                    type: "list",
-                    name: "department",
-                    message: "Please select the employee's department",
-                    choices: [
-                        "Sales",
-                        "Fixed Operations",
-                        "Office",
-                    ]
-                }
-            ])
+            // [
+
+            //     // {
+            //     //     type: "list",
+            //     //     name: "department",
+            //     //     message: "Please select the employee's department",
+            //     //     choices: [
+            //     //         "Sales",
+            //     //         "Fixed Operations",
+            //     //         "Office",
+            //     //     ]
+            //     // }
+            // ])
             .then((employee) => {
-                // let id = uuid.v4();
                 let insertEmp = "INSERT INTO employee SET ?";
                 let empValues = {
                     first_name: employee.first_name,
                     last_name: employee.last_name,
                 };
-                database.query(insertEmp, empValues, (err, res) => {
-                    if (err) throw (err);
-                    console.log('name inserted');
-                })
-                //////
-                let insertRole = "INSERT INTO role SET ?";
-                let empRole = {
-                    title: employee.title,
-                    salary: employee.salary,
-                };
-                database.query(insertRole, empRole, (err, res) => {
-                    if (err) throw (err);
-                    console.log('title inserted');
-                })
-                //////
-                let insertDept = "INSERT INTO department SET ?"
-                let empDept = {
-                    name: employee.department,
-                }
-                database.query(insertDept, empDept, (err, res) => {
-                    if (err) throw (err);
-                    console.log('dept inserted');
-                })
+                // database.query(insertEmp, empValues, (err, res) => {
+                //     if (err) throw (err);
+                //     console.log('name inserted');
+                // })
+                // //////
+                // let insertRole = "INSERT INTO role SET ?";
+                // let empRole = {
+                //     title: employee.title,
+                //     salary: employee.salary,
+                // };
+                // database.query(insertRole, empRole, (err, res) => {
+                //     if (err) throw (err);
+                //     console.log('title inserted');
+                // })
+                // //////
+                // let insertDept = "INSERT INTO department SET ?"
+                // let empDept = {
+                //     name: employee.department,
+                // }
+                // database.query(insertDept, empDept, (err, res) => {
+                //     if (err) throw (err);
+                //     console.log('dept inserted');
+                // })
             });
     };
+    updateEMP() {
+        inquirer.prompt(
+
+            {
+                type: "number",
+                name: "salary",
+                message: "Please enter the employee's salary.",
+            },
+        )
+    }
 };
 
 module.exports = new DbMethod;
