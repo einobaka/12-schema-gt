ff72bed4fc983678407267a41691f3ee3fe97c53
einobaka <61556425+einobaka@users.noreply.github.com>
2020-08-30 21:10:33 -0700

    removed password from mysql

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const database = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '!Q2w3e123',
+    password: '',
     database: 'employeeDB',
 
 });
